9fc6f2c15e6effb23076b44f3a9e5cccb696265d
Jan Murre <jan.murre@gmail.com>
2019-10-30 16:15:17 +0100

    Mangling with the api prefix, added CORS.

--- dynapi/run.sh
+++ dynapi/run.sh
@@ -1,5 +1,6 @@
 export FLASK_APP=dynapi/wsgi.py 
 export FLASK_ENV=development 
 export ROUTES_ROOT_DIR=./data 
+export URI_PATH=/
 export DATABASE_URL=postgres://postgres:@localhost:5434/postgres
 bin/flask run -p 5001
