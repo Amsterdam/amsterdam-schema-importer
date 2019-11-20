c2483f0f6d2c6eaa489ce4592706caa036193db9
Jan Murre <jan.murre@gmail.com>
2019-11-20 16:23:20 +0100

    Changed some ports, more in line with openstack approach.

--- dynapi/run.sh
+++ dynapi/run.sh
@@ -1,7 +1,8 @@
 export FLASK_APP=dynapi/app.py 
 export FLASK_ENV=development 
 export ROUTES_ROOT_DIR=./data 
-export SCHEMA_REPO_URL=https://api.github.com/repos/jjmurre/schematest
+# export SCHEMA_REPO_URL=https://api.github.com/repos/jjmurre/schematest
+export SCHEMA_REPO_URL=https://api.github.com/repos/Amsterdam/schemas
 export URI_PATH=/
 export DATABASE_URL=postgres://postgres:@localhost:5434/postgres
 bin/flask run -p 5001
