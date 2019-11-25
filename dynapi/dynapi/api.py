1a9b1143342b7a93b026a2a2715b9c77a86d1ff0
Jan Murre <jan.murre@gmail.com>
2019-11-25 15:16:20 +0100

    Try path with hardcoded vars.

--- dynapi/dynapi/api.py
+++ dynapi/dynapi/api.py
@@ -182,7 +182,8 @@ make_routes(routes_root_dir)
 
 @api.route("/")
 def index():
-    openapi_spec_path = f"{uri_path}spec"
+    # openapi_spec_path = f"{uri_path}spec"
+    openapi_spec_path = f"/v1/spec"
     return render_template("index.html", openapi_spec_path=openapi_spec_path)
 
 
