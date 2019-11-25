bfabed4a461d87ac1befe88f409fe425550fa5ef
Jan Murre <jan.murre@gmail.com>
2019-11-25 14:03:09 +0100

    Fixes to api_path.

--- dynapi/run.sh
+++ dynapi/run.sh
@@ -1,7 +1,7 @@
 export FLASK_APP=dynapi/app.py 
 export FLASK_ENV=development 
 export ROUTES_ROOT_DIR=./data 
-export URI_PATH=/
+export URI_PATH=/api/
 export DATABASE_URL=postgres://postgres:@localhost:5434/postgres
 bin/flask run -p 5001
 # bin/python dynapi/wsgi.py
