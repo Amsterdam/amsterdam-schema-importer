c5a2a9d0f294e76d5ddfcd41ae8f296e123e29fd
Jan Murre <jan.murre@gmail.com>
2019-12-19 15:08:55 +0100

    Add SCHEMA_URL for local flask runner scripts.

--- worker/run.sh
+++ worker/run.sh
@@ -1,4 +1,5 @@
 export FLASK_APP=worker/web.py 
 export FLASK_ENV=development 
+export SCHEMA_URL=https://schemas.data.amsterdam.nl/datasets/
 flask run -p 5002 --host=0.0.0.0
 # python -m pdb -m flask run -p 5002
